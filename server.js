21d3ccc9bff972c7c20712142e6d81b7345dc899
Prathamesh  Thakare <prathamthackeray18@gmail.com>
2026-02-15 21:26:21 +0530

    added image generation and content response functionality in Discord bot

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, AttachmentBuilder } = require('discord.js');
+const { GoogleGenAI } = require("@google/genai");
+const fs = require('fs');
 
 
 const client = new Client ({
@@ -10,19 +12,55 @@ const client = new Client ({
     ]
 })
 
-client.once("ready", () => {
+const ai = new GoogleGenAI({
+    apiKey: process.env.GOOGLE_API_KEY
+})
+
+async function generateImage(prompt) {
+    const response = await ai.models.generateContent({
+        model: "gemini-2.5-flash",
+        contents: prompt,
+    });
+
+
+    for (const part of response.candidates[0].content.parts) {
+        if (part.text) {
+            console.log(part.text);
+        } else if (part.inlineData) {
+            const imageData = part.inlineData.data;
+            const buffer = Buffer.from(imageData, "base64");
+            return buffer;
+        }
+    }
+}
+
+async function generateContent(prompt) {
+    const response = await ai.models.generateContent({
+        model: "gemini-2.5-flash",
+        contents: prompt,
+    });
+    return response.text;
+}
+    
+
+
+
+
+client.once("clientReady", () => {
     console.log("Bot is ready!");
 } )
 
-client.on("messageCreate", (message)=> {
+client.on("messageCreate", async (message)=> {
     // console.log(`message received: ${message.content}`)
     // console.log(message.author)
 
     const isBot = message.author.bot;
     if(isBot)return;
 
-    message.reply("Hello, How i can asist you?") 
-    
+    const content = await generateContent(message.content);
+    if(content){
+        message.reply(content);
+    }
 
 })
 
